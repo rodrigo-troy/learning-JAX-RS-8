4054f9778a9d776716328697a65f5614ae5d96ef
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2024-11-24 21:53:55 -0300

    Refactor Operation enum to use BinaryOperator instead of BiFunction.
    Replaces BiFunction with BinaryOperator for better semantic clarity. Also reorders the operations within the enum for improved readability, ensuring all operations are processed in a consistent manner.

--- src/main/java/com/rodrigotroy/learningjaxrs/domain/Operation.java
+++ src/main/java/com/rodrigotroy/learningjaxrs/domain/Operation.java
@@ -2,7 +2,7 @@ package com.rodrigotroy.learningjaxrs.domain;
 
 import javax.xml.bind.annotation.XmlEnum;
 import javax.xml.bind.annotation.XmlTransient;
-import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,24 +14,25 @@ import java.util.function.BiFunction;
 @XmlEnum
 public enum Operation {
     ADD(Double::sum),
-    SUBTRACT((a, b) -> a - b),
-    MULTIPLY((a, b) -> a * b),
     DIVIDE((a, b) -> {
         if (b == 0) {
             throw new ArithmeticException("Division by zero");
         }
+
         return a / b;
-    });
+    }),
+    MULTIPLY((a, b) -> a * b),
+    SUBTRACT((a, b) -> a - b);
 
     @XmlTransient
-    private final BiFunction<Double, Double, Double> operation;
+    private final BinaryOperator<Double> function;
 
-    Operation(BiFunction<Double, Double, Double> operation) {
-        this.operation = operation;
+    Operation(BinaryOperator<Double> function) {
+        this.function = function;
     }
 
     @XmlTransient
     public Double apply(Double a, Double b) {
-        return operation.apply(a, b);
+        return function.apply(a, b);
     }
 }
