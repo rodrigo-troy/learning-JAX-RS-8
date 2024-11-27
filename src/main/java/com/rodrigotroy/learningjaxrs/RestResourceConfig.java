4c838d2ac459e2fd3d2aeb6780dae3652e8cd2b4
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2024-11-27 12:52:02 -0300

    Refactor RestResourceConfig for better readability
    Consolidated package name into a constant for reuse and improved readability. Reformatted long property lines for better code clarity and maintainability.

--- src/main/java/com/rodrigotroy/learningjaxrs/RestResourceConfig.java
+++ src/main/java/com/rodrigotroy/learningjaxrs/RestResourceConfig.java
@@ -7,9 +7,13 @@ import javax.ws.rs.ApplicationPath;
 
 @ApplicationPath("/api")
 public class RestResourceConfig extends ResourceConfig {
+    private static final String PACKAGE = "com.rodrigotroy.learningjaxrs";
+
     public RestResourceConfig() {
-        packages("com.rodrigotroy.learningjaxrs");
-        property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
-        property(ServerProperties.BV_DISABLE_VALIDATE_ON_EXECUTABLE_OVERRIDE_CHECK, true);
+        packages(PACKAGE);
+        property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE,
+                 true);
+        property(ServerProperties.BV_DISABLE_VALIDATE_ON_EXECUTABLE_OVERRIDE_CHECK,
+                 true);
     }
 }
